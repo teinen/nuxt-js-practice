7c609fa0b12b776ca9ba6785f4f89c149df6f72e
諦念 <teinen.github@gmail.com>
2018-08-19 22:58:26 +0900

    Add nested routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,27 @@ module.exports = {
   },
   router: {
     middleware: ['visits', 'user-agent']
-  }
+  },
+  env: {
+    users: [
+      { id: 1, name: 'Kobe Bryant', number: 24 },
+      { id: 2, name: 'Michael Jordan', number: 23 },
+      { id: 3, name: 'Stephen Curry', number: 30 },
+      { id: 4, name: 'Lebron James', number: 23 },
+      { id: 5, name: 'Kevin Durant', number: 35 },
+      { id: 6, name: 'Kyrie Irving', number: 2 }
+    ]
+  },
+  generate: {
+    routes: [
+      '/1',
+      '/2',
+      '/3',
+      '/4',
+      '/5',
+      '/6'
+    ]
+  },
+  loading: false
   // loading: '~/components/loading.vue'
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="container">
-    <div class="content">
+    <!-- <div class="content">
       <h1 class="title">Custom CSS!</h1>
       <p><nuxt-link to="/about" class="button is-medium is-primary hvr-float-shadow">I am a button</nuxt-link></p>
       <p><nuxt-link to="/about">About page</nuxt-link></p>
-    </div>
+    </div> -->
 
-    <h2>Users</h2>
-    <ul class="users">
-      <li v-for="user in users" :key="user.id">
-        <nuxt-link :to="'/users/'+user.id">{{ user.name }}</nuxt-link>
-      </li>
-    </ul>
+    <div class="left">
+      <h2><nuxt-link to="/">Players</nuxt-link></h2>
+      <ul class="players">
+        <li v-for="user in users" :key="user.id">
+          <nuxt-link :to="'/'+user.id">{{ user.name }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <div class="right">
+      <nuxt-child :key="$route.params.id"/>
+    </div>
   </div>
 </template>
 
@@ -19,32 +24,57 @@
 import axios from 'axios'
 
 export default {
-  async asyncData () {
+  async asyncData ({ env }) {
     const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users`)
-    return { users: data }
+    return { users: env.users }
   }
 }
 </script>
 
 <style scoped>
+.page-enter-active, .page-leave-active {
+  transition: opacity .4s, transform .4s;
+  transform-style: preserve-3d;
+  backface-visibility: hidden;
+  opacity: 1;
+}
+.page-enter, .page-leave-active {
+  opacity: 0.5;
+  transform: rotateY(100deg);
+}
 .container {
-  text-align: center;
-  margin-top: 100px;
+  width: 100%;
+  margin: 0;
+  padding: 0;
   font-family: sans-serif;
+  box-sizing: border-box;
 }
-.users {
+.left {
+  width: 50%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.right {
+  width: 50%;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.players {
   list-style-type: none;
+  margin: 0;
+  padding: 0;
 }
-.users li a {
-  display: inline-block;
-  width: 200px;
+.players li a {
+  display: block;
   border: 1px #ddd solid;
   padding: 10px;
   text-align: left;
   color: #222;
   text-decoration: none;
 }
-.users li a:hover {
+.players li a:hover {
   color: orange;
 }
 </style>
